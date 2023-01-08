d1049f17a08c562a239c9f97a45176ccb4f2ab10
ramitules <ramirotules@hotmail.com>
2023-01-08 17:33:07 -0300

    s

--- Main.py
+++ Main.py
@@ -10,21 +10,21 @@ ventana.wm_title('Facturacion textil')
 
 ventana.mainloop()
 
-while True:
-    os.system("cls")
-    print("Bienvenido!\n")
-    print("Elija una opcion")
-    print("1. Facturas")
-    print("2. Clientes")
-    print("3. Articulos")
-    print("0. Salir")
-    print("\nOpcion: ", end = '')
-    opc = int(input())
+#while True:
+#    os.system("cls")
+#    print("Bienvenido!\n")
+#    print("Elija una opcion")
+#    print("1. Facturas")
+#    print("2. Clientes")
+#    print("3. Articulos")
+#    print("0. Salir")
+#    print("\nOpcion: ", end = '')
+#    opc = int(input())
 
-    if opc == 0: break
-    elif opc == 1: menu_facturas()
-    elif opc == 2: menu_clientes()
-    elif opc == 3: menu_articulos()
-    else: input('Opcion incorrecta, presione ENTER y vuelva a intentar')
+#    if opc == 0: break
+#    elif opc == 1: menu_facturas()
+#    elif opc == 2: menu_clientes()
+#    elif opc == 3: menu_articulos()
+#    else: input('Opcion incorrecta, presione ENTER y vuelva a intentar')
 
-input('Gracias por utilizar este programa! Presione ENTER para salir')
+#input('Gracias por utilizar este programa! Presione ENTER para salir')
